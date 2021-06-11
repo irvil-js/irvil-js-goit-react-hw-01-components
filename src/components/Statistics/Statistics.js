07fb048febb8fe73335c0f155c8108afed939c5e
Irina Murashka <irvil@tut.by>
2021-06-11 17:38:35 +0100

    стили Statistics

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -6,10 +6,10 @@ const Statistics = ({ title, stats }) => (
   <section className="statistics">
     <h2 className="title">{title}</h2>
 
-    <ul className="stat-list">
+    <ul className="stat_list">
       {stats.map((stat) => (
         <li className="item">
-          <span className="label">.{stat.label}</span>
+          <span className="label">{stat.label}</span>
           <span className="percentage">{stat.percentage}%</span>
         </li>
       ))}
